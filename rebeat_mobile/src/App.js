fe20cb2d63201dad962143693a0e2901c6414afe
BBIYAKYEE7 <dadalgojr@gmail.com>
2024-05-03 21:13:42 +0900

    Update App.js

--- rebeat_mobile/src/App.js
+++ rebeat_mobile/src/App.js
@@ -1,17 +1,14 @@
-import React, { useEffect, useState, useRef} from 'react';
-import { AreaChart, Area, Tooltip } from 'recharts';
+import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
 import Pusher from 'pusher-js';
 import logo from './logo.png';
 import icon from './location.png';
-import text_logo from './text_logo.png';
 import './App.css';
 
-function MobilePage() {
+function App() {
   const [data, setData] = useState({ score: [], depth: 0, pressure: 0, cycle: 0, elapsed_time: 0 });
   const minutes = Math.floor(data.elapsed_time / 60);
   const seconds = data.elapsed_time % 60;
-
-  const scrollRef = useRef();
 
   useEffect(() => {
     document.body.style.overflow = 'hidden'
@@ -39,25 +36,46 @@ function MobilePage() {
     };
   }, []);
 
-  const CustomTooltip = ({ active, payload, label }) => {
-    if (active && payload && payload.length) {
-      return (
-        <div className="custom-tooltip">
-          <p className="label">{`Time : ${label}`}</p>
-          <p className="intro">{`Score : ${payload[0].value}`}</p>
-        </div>
-      );
-    }
-  
-    return null;
+  const chartData = {
+    labels: data.score.map((_, index) => index + 1),
+    datasets: [
+      {
+        label: 'Score',
+        data: data.score,
+        fill: false,
+        backgroundColor: 'rgb(107, 98, 241)',
+        borderColor: 'rgba(107, 98, 241, 0.2)',
+      },
+    ],
   };
 
-  useEffect(() => {
-    // Scroll to the start of the div whenever data.score changes
-    if (scrollRef.current) {
-      scrollRef.current.scrollLeft = scrollRef.current.scrollWidth;
-    }
-  }, [data.score]);
+  const options = {
+    responsive: true,
+    plugins: {
+      tooltip: {
+        enabled: true,
+      },
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        display: true,
+        title: {
+          display: true,
+          text: 'Time',
+        },
+      },
+      y: {
+        display: true,
+        title: {
+          display: true,
+          text: 'Score',
+        },
+      },
+    },
+  };
 
   return (
     <div>
@@ -71,27 +89,11 @@ function MobilePage() {
       <p style={{ marginTop: '12px', width: '280px', marginLeft: '40px', wordSpacing: '-2%' }}>Composite CPR Score: {data.score[data.score.length - 1]}</p>
       <p style={{ marginTop: '10px', width: '280px', marginLeft: '40px', wordSpacing: '-2%' }}>Compression Depth: {data.depth}cm</p>
       <p style={{ marginTop: '10px', width: '280px', marginLeft: '40px', wordSpacing: '-2%' }}>Compression Cycle: {data.cycle}bpm</p>
-      <div style={{ position: 'relative', overflowX: 'scroll' }} ref={scrollRef}> {/* Change scrollRefq to scrollRef */}
-        <AreaChart
-          width={Math.max(window.innerWidth, data.score.length * 100)} // Set the width dynamically based on the number of data points
-          height={370}
-          data={data.score.map((score, index) => ({ time: index + 1, score }))}
-          margin={{
-            top: 10,
-            right: 0,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CustomTooltip />
-          <Tooltip />
-          <Area type="monotone" dataKey="score" stroke="#6B62F1" fill="#6B62F1" isAnimationActive={false} />
-        </AreaChart>
-        <img src={text_logo} alt="text_logo" style={{ position: 'fixed', top: '88%', left: '58%', height: '2em', width: '6.5em' }} />
-        <q style={{ position: 'fixed', top: '65%', left: '25%', fontSize: '1em', color: '#FFFFFF', opacity: '50%' }}>Score change trend graph</q>
+      <div style={{ position: 'relative', overflowX: 'scroll' }}>
+        <Line data={chartData} options={options} />
       </div>
     </div>
   );
 }
 
-export default MobilePage; 
+export default App;

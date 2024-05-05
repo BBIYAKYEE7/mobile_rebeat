4dffd2156b018002956bc4a33dc12c237a5a535b
BBIYAKYEE7 <dadalgojr@gmail.com>
2024-05-06 00:39:39 +0900

    Update App.js

--- rebeat_mobile/src/App.js
+++ rebeat_mobile/src/App.js
@@ -101,7 +101,7 @@ function MobilePage() {
       <div style={{ position: 'relative', overflowX: 'scroll'}} ref={scrollRef}> { }
         <ComposedChart
           width={Math.max(window.innerWidth, data.depth.length * 100)} // Set the width dynamically based on the number of data points
-          height={300}
+          height={450}
           data={data.depth.map((depth, index) => ({ time: index + 1, depth, score: data.score[index] }))}
           margin={{
             top: 30,

68e02f5b987b53b1532db432b6b599fcbd7e25b3
Jeayoon Kim <73587928+BBIYAKYEE7@users.noreply.github.com>
2024-05-05 16:11:51 +0900

    Update App.js

--- rebeat_mobile/src/App.js
+++ rebeat_mobile/src/App.js
@@ -91,7 +91,7 @@ function MobilePage() {
           height={400}
           data={data.score.map((score, index) => ({ time: index + 1, score }))}
           margin={{
-            top: 30,
+            top: 10,
             right: 0,
             left: 0,
             bottom: 0,

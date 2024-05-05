dd59ff0f2ccb03dd7d2881194454832f16b3f214
BBIYAKYEE7 <dadalgojr@gmail.com>
2024-05-06 01:54:54 +0900

    Update App.js

--- rebeat_mobile/src/App.js
+++ rebeat_mobile/src/App.js
@@ -78,8 +78,8 @@ function MobilePage() {
         <h1 style={{ fontSize: '22.01px', marginTop: '53px', wordSpacing: '-2%', fontWeight: '600' }}>Los Angeles Convention Center</h1>
       </div>
       <p style={{ fontSize: '22.26px', marginTop: '15%', width: '150px', marginLeft: '40px', wordSpacing: '-2%' }}>Elapsed time</p>
-      <p style={{ fontSize: '22.26px', fontWeight: '600', width: '150px', marginTop: '-20px', marginLeft: '40px', wordSpacing: '-2%' }}>{`${minutes.toString().padStart(2, '0')}min ${seconds.toString().padStart(2, '0')}sec`}</p>
-      <h2 style={{ fontSize: '22.89px', marginTop: '34px', fontWeight: '600', width: '330px', marginLeft: '40px', wordSpacing: '-2%' }}>Real-time averages</h2>
+      <p style={{ fontSize: '22.26px', fontWeight: 'bold', width: '150px', marginTop: '-20px', marginLeft: '40px', wordSpacing: '-2%' }}>{`${minutes.toString().padStart(2, '0')}min ${seconds.toString().padStart(2, '0')}sec`}</p>
+      <h2 style={{ fontSize: '22.89px', marginTop: '34px', fontWeight: 'bold', width: '330px', marginLeft: '40px', wordSpacing: '-2%' }}>Real-time averages</h2>
       <p style={{ fontSize: '22.26px', marginTop: '-2px', width: '330px', marginLeft: '40px', wordSpacing: '-2%' }}>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <div style={{ marginRight: '10px' }}>Composite CPR Score:</div>
